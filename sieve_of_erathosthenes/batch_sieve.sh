d02efebaf716d312758581ebedd01a3dd7ed6e05
asinha94 <anuraagsinha@hotmail.com>
2018-03-23 19:15:38 -0700

    added scripts

---
+++ sieve_of_erathosthenes/batch_sieve.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mpirun -np "$1" /project/ece-ugrad/codesub/asinha/sieve "$2"
---
+++ sieve_of_erathosthenes/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    exit "Need 2 Arguments. ./batch_sieve <CPUS> <NUMBERS_TO_ITERATE_THROUGH>"
+fi
+
+# Arguments
+CPUS=$1
+N=$2
+
+make build
+
+qsub -q cluster-short -1 nodes=1:ppn="$CPUS" "./batch_sieve.sh $CPUS $N"
